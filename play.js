6acc096e00d91115537b49c37689951d4c0b565c
William Gadd <wgadd@sasktel.net>
2022-09-28 19:44:23 +0000

    Moved input-related commands from play.js to a new file input.js

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const connect = require ('./client').connect;
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", key => handleUserInput(key));
-  
-  return stdin;
-};
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
